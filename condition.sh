47b1689475ae2ea2b53eef75f300420325572101
Harsha Reddy <harshavardhan7004@gmail.com>
2026-02-17 19:17:52 +0530

    practise

--- condition.sh
+++ condition.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-read NUM
-
 echo "enter number:$NUM"
 
-if [$NUM>10]; then
+read NUM
+
+if [ $NUM -gt 10 ]; then
 
 echo "$NUM is greater than 10"
 
-elif [$NUM<10];then
+elif [ $NUM<10 ];then
 echo "$NUM is less than 10"
 
 fi
