1b82e987b61ad4d3ceb3a1fe2347c1eccf96b0d2
Harsha Reddy <harshavardhan7004@gmail.com>
2026-02-17 20:32:33 +0530

    practise

--- exitcode.sh
+++ exitcode.sh
@@ -4,10 +4,20 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
     echo "please try in root directory"
+
+    exit 1
 fi
 
-exit 1
 
-echo "ngnix installing..."
+echo "ngnix install ....."
 
 dnf install ngnix -y
+
+if [ $? -ne 0 ]; then
+
+echo "ngnix install .....failure"
+exit 1
+
+else 
+
+echo "installing ngnix....Success"
