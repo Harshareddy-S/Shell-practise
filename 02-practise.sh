5d9c1b4ba87cbca22bf302420390af75d397cde9
Harsha Reddy <harshavardhan7004@gmail.com>
2026-02-17 18:27:27 +0530

    practise

--- 02-practise.sh
+++ 02-practise.sh
@@ -3,8 +3,10 @@
 echo "Enter your name::"
 read USER_NAME
 
-echo "enter password"
-read -s USER_NAME
-
 echo "user name is $USER_NAME"
-echo "password is $USER_NAME"
+
+echo "Enter passowrd:"
+read password
+
+echo "password is $PASSWORD"
+
