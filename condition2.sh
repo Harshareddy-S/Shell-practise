e46774a4c7a4684673157b98cd310f620f0ebc33
Harsha Reddy <harshavardhan7004@gmail.com>
2026-02-17 20:46:30 +0530

    practise

--- condition2.sh
+++ condition2.sh
@@ -3,11 +3,13 @@
 NUM=$1
 
 if [ $NUM -gt 20 ]; then
-echo "$NUM is greater than 20 "
+echo "$NUM is greater than 20"
 
-elif [$NUM -lt 20 ]; then
+elif [ $NUM -lt 20 ]; then
 echo "$NUM is less than 20"
 
-else "Given number is 20"
+else 
+
+echo "Given number is 20"
 
 fi
