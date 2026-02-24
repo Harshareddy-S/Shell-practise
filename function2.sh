ec40bae6818e16bfa75f9902159991815529feb2
Harsha Reddy <harshavardhan7004@gmail.com>
2026-02-24 12:02:49 +0530

    changed something

--- function2.sh
+++ function2.sh
@@ -1,6 +1,6 @@
 USERID=$(id -u)
-LOGS_FOLDER= "var/log/Shell-practise"
-LOGS_FILE="var/log/Shell-practise/$0.log"
+LOGS_FOLDER= "/var/log/Shell-practise"
+LOGS_FILE="/var/log/Shell-practise/$0.log"
 
 if [ $USERID -ne 0 ]; then
     echo "Try in root directory"
