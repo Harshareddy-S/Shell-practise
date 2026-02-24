36983b2b4ca5ead76cdc1b6a532494e563d03174
Harsha Reddy <harshavardhan7004@gmail.com>
2026-02-24 12:50:29 +0530

    changed something

---
+++ loops2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Try in root directory" |tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo "$2 ...Failure" |tee -a $LOGS_FILE
+    exit 1
+else
+    echo "$2 ... Success" |tee -a $LOGS_FILE
+fi
+}
+
+for package in $@
+do 
+    echo "dnf list installed $package"
+    if[ $? -ne 0 ]; then
+
+        echo "$package is not installed..instlling now" &>> $LOGS_FILE
+        dnf install $package -y &>> $LOGS_FILE
+        VALIDATE $? "$package intalling"
+
+    else
+        echo ""$package already installed.. skipping installation  &>> $LOGS_FILE
+
+done
