c9fd7cfc4d1a8e72e3b02d1c2fc69c1505d8d7a5
Harsha Reddy <harshavardhan7004@gmail.com>
2026-02-24 10:57:06 +0530

    practise

---
+++ function.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+
+    echo "Try in root directory"
+
+    exit 1
+
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+
+    echo "nginx installing ...Failure"
+    exit 1
+else
+
+    echo "nginx isntalling ... Success"
+
+fi
+
+
+dnf install Mysql -y
+
+if [ $? -ne 0 ]; then
+
+    echo "Mysql installing ...Failure"
+    exit 1
+else
+
+    echo "Mysql isntalling ... Success"
+
+fi
+
+dnf install mongodb -y
+
+if [ $? -ne 0 ]; then
+
+    echo "mongodb installing ...Failure"
+    exit 1
+else
+
+    echo "mongodb isntalling ... Success"
+
+fi
